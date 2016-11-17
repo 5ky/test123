5ab06a2e7fcb4a0abffaedbb1981e3e326ac3154
5ky <marius.szoprun@gmx.de>
2016-11-17 15:51:46 +0100

    Initial commit

---
+++ src/main/java/mein/packet/name/test123.java
@@ -0,0 +1,16 @@
+package mein.packet.name;
+
+
+import ch.qos.logback.core.Appender;
+
+/**
+ * Created by infinity on 16.11.2016.
+ */
+public class test123 {
+
+    public static void main(String[] args) {
+        System.out.println("1Hello World!");
+        Appender a;
+    }
+
+}
